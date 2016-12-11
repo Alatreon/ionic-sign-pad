23dfbee44b38533d178b55b884c619de883c708f
Alateon <dc.shoes@hotmail.fr>
2016-12-11 18:51:06 +0100

    dev updated bug corr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,24 +4,24 @@ import { MyApp } from './app.component';
 import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
 import { ItemDetailsPage } from '../pages/item-details/item-details';
 import { ListPage } from '../pages/list/list';
-
+import { HomePage } from '../pages/home/home';
+import { SignaturePadModule } from 'angular2-signaturepad';
+import { Storage } from '@ionic/storage';
+ 
 @NgModule({
   declarations: [
     MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
+    HomePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    SignaturePadModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HelloIonicPage,
-    ItemDetailsPage,
-    ListPage
+    HomePage
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  providers: [Storage]
 })
 export class AppModule {}
