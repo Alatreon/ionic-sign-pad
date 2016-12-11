801a27e0e9fd0568123f22203a664e912d185e0d
Alateon <dc.shoes@hotmail.fr>
2016-12-11 19:35:16 +0100

    correction de quelque 'warn'.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
-import { HelloIonicPage } from '../pages/hello-ionic/hello-ionic';
-import { ItemDetailsPage } from '../pages/item-details/item-details';
-import { ListPage } from '../pages/list/list';
-import { HomePage } from '../pages/home/home';
+import { HomePage } from '../pages/hello-ionic/hello-ionic';
 import { SignaturePadModule } from 'angular2-signaturepad';
 import { Storage } from '@ionic/storage';
  
